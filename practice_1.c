5636c0ff1cc7026b322167cd783d9f2164fdacfb
JaynalAbdin <mjabdin.311@gmail.com>
2023-04-04 01:15:10 +0600

    practice problem 1

---
+++ practice_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Recently I heard that you've achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you will shine in your life!\t\tGood luck with all the bariers\(/\\) in your life.");
+
+    return 0;
+}
